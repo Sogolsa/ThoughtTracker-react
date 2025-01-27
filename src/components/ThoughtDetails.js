77d88265fd617cc58ff3f1aa82c6a1153fb668f7
Sogol <sogol.saadat@gmail.com>
2025-01-26 20:15:42 -0600

    navigate to /thoughts after updating details

--- src/components/ThoughtDetails.js
+++ src/components/ThoughtDetails.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import {
   TextField,
   Button,
@@ -7,18 +7,18 @@ import {
   Typography,
   Box,
   Grid,
-} from '@mui/material';
+} from "@mui/material";
 
 const ThoughtDetails = ({ token }) => {
   const { thoughtId } = useParams();
   const navigate = useNavigate();
   const [thought, setThought] = useState(null);
   const [formData, setFormData] = useState({
-    Description: '',
+    Description: "",
     Emotions: [],
     Problems: [],
     possibleSolutions: [],
-    Affirmation: '',
+    Affirmation: "",
   });
 
   useEffect(() => {
@@ -28,7 +28,7 @@ const ThoughtDetails = ({ token }) => {
         const response = await fetch(
           `https://thought-tracker-journal-4688a4169626.herokuapp.com/thoughts/${thoughtId}`,
           {
-            method: 'GET',
+            method: "GET",
             headers: {
               Authorization: `Bearer ${token}`,
             },
@@ -39,17 +39,17 @@ const ThoughtDetails = ({ token }) => {
           const data = await response.json();
           setThought(data);
           setFormData({
-            Description: data.Description || '',
+            Description: data.Description || "",
             Emotions: data.Emotions || [],
             Problems: data.Problems || [],
             possibleSolutions: data.possibleSolutions || [],
-            Affirmation: data.Affirmation || '',
+            Affirmation: data.Affirmation || "",
           });
         } else {
-          console.error('Failed to fetch thought:', response.statusText);
+          console.error("Failed to fetch thought:", response.statusText);
         }
       } catch (error) {
-        console.error('Error fetching thought:', error);
+        console.error("Error fetching thought:", error);
       }
     };
 
@@ -70,9 +70,9 @@ const ThoughtDetails = ({ token }) => {
       const response = await fetch(
         `https://thought-tracker-journal-4688a4169626.herokuapp.com/thoughts/${thoughtId}`,
         {
-          method: 'PUT',
+          method: "PUT",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify(formData),
@@ -82,12 +82,13 @@ const ThoughtDetails = ({ token }) => {
       if (response.ok) {
         const updatedThought = await response.json();
         setThought(updatedThought);
-        alert('Thought updated successfully');
+        alert("Thought updated successfully");
+        navigate("/thoughts");
       } else {
-        alert('Failed to update thought');
+        alert("Failed to update thought");
       }
     } catch (error) {
-      console.error('Error updating thought:', error);
+      console.error("Error updating thought:", error);
     }
   };
 
@@ -96,7 +97,7 @@ const ThoughtDetails = ({ token }) => {
       const response = await fetch(
         `https://thought-tracker-journal-4688a4169626.herokuapp.com/thoughts/${thoughtId}`,
         {
-          method: 'DELETE',
+          method: "DELETE",
           headers: {
             Authorization: `Bearer ${token}`,
           },
@@ -104,97 +105,97 @@ const ThoughtDetails = ({ token }) => {
       );
 
       if (response.ok) {
-        alert('Thought deleted successfully');
-        navigate('/thoughts');
+        alert("Thought deleted successfully");
+        navigate("/thoughts");
       } else {
-        alert('Failed to delete thought');
+        alert("Failed to delete thought");
       }
     } catch (error) {
-      console.error('Error deleting thought:', error);
+      console.error("Error deleting thought:", error);
     }
   };
 
   if (!thought) return <Typography>Loading...</Typography>;
 
   return (
-    <Container maxWidth='md' sx={{ textAlign: 'center' }}>
-      <Typography variant='h4' sx={{ mt: 3 }} gutterBottom>
+    <Container maxWidth="md" sx={{ textAlign: "center" }}>
+      <Typography variant="h4" sx={{ mt: 3 }} gutterBottom>
         Thought Details
       </Typography>
 
       {/* Display the thought name and created date */}
       <Box
         sx={{
-          backgroundColor: '#fff',
+          backgroundColor: "#fff",
           padding: 4,
           borderRadius: 2,
-          boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)',
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
         }}
       >
         <Box sx={{ mb: 3 }}>
-          <Typography variant='h6'>
+          <Typography variant="h6">
             Thought Name: {thought.thoughtName}
           </Typography>
-          <Typography variant='body2' color='textSecondary'>
+          <Typography variant="body2" color="textSecondary">
             Created on: {new Date(thought.created_date).toLocaleString()}
           </Typography>
         </Box>
 
         {/* Form for updating thought details */}
-        <Box component='form' onSubmit={handleUpdate}>
+        <Box component="form" onSubmit={handleUpdate}>
           <TextField
             fullWidth
-            label='Edit Thought Name'
-            name='thoughtName'
+            label="Edit Thought Name"
+            name="thoughtName"
             value={formData.thoughtName}
             multiline
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
           />
           <TextField
             fullWidth
-            label='Description'
-            name='Description'
+            label="Description"
+            name="Description"
             value={formData.Description}
             multiline
             // minRows={10}
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
           />
           <TextField
             fullWidth
-            label='Emotions'
-            name='Emotions'
+            label="Emotions"
+            name="Emotions"
             value={formData.Emotions}
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
             multiline
           />
           <TextField
             fullWidth
-            label='Problems'
-            name='Problems'
+            label="Problems"
+            name="Problems"
             value={formData.Problems}
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
             multiline
           />
           <TextField
             fullWidth
-            label='Possible Solutions'
-            name='possibleSolutions'
+            label="Possible Solutions"
+            name="possibleSolutions"
             value={formData.possibleSolutions}
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
             multiline
           />
           <TextField
             fullWidth
-            label='Affirmation'
-            name='Affirmation'
+            label="Affirmation"
+            name="Affirmation"
             value={formData.Affirmation}
             onChange={handleChange}
-            margin='normal'
+            margin="normal"
             multiline
           />
 
@@ -202,17 +203,17 @@ const ThoughtDetails = ({ token }) => {
           <Grid
             container
             spacing={1}
-            sx={{ mt: 2, justifyContent: 'flex-end' }}
+            sx={{ mt: 2, justifyContent: "flex-end" }}
           >
             <Grid item>
               <Button
-                type='submit'
-                variant='contained'
-                color='primary'
+                type="submit"
+                variant="contained"
+                color="primary"
                 sx={{
                   mt: 2,
-                  backgroundColor: '#2c4e51',
-                  '&:hover': { backgroundColor: '#2c3e50' },
+                  backgroundColor: "#2c4e51",
+                  "&:hover": { backgroundColor: "#2c3e50" },
                 }}
               >
                 Update Thought
@@ -220,8 +221,8 @@ const ThoughtDetails = ({ token }) => {
             </Grid>
             <Grid item>
               <Button
-                variant='outlined'
-                color='error'
+                variant="outlined"
+                color="error"
                 sx={{ mt: 2 }}
                 onClick={handleDelete}
               >
