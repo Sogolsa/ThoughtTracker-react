542d00a17fae46e247347a19a08eac09acef234c
Sogol <sogol.saadat@gmail.com>
2025-02-28 13:23:11 -0600

    Added show details after adding for better readability

--- src/components/ThoughtDetails.js
+++ src/components/ThoughtDetails.js
@@ -14,6 +14,7 @@ const ThoughtDetails = ({ token }) => {
   const { thoughtId } = useParams();
   const navigate = useNavigate();
   const [thought, setThought] = useState(null);
+  const [showDetails, setShowDetails] = useState(false);
   const [formData, setFormData] = useState({
     Description: "",
     Emotions: [],
@@ -43,6 +44,9 @@ const ThoughtDetails = ({ token }) => {
             possibleSolutions: data.possibleSolutions || [],
             Affirmation: data.Affirmation || "",
           });
+          if (data.thoughtName) {
+            setShowDetails(true);
+          }
         } else {
           console.error("Failed to fetch thought:", response.statusText);
         }
@@ -77,9 +81,10 @@ const ThoughtDetails = ({ token }) => {
       if (response.ok) {
         const updatedThought = await response.json();
         setThought(updatedThought);
+        setShowDetails(true);
 
         alert("Thought updated successfully");
-        navigate("/thoughts");
+        // navigate("/thoughts");
       } else {
         alert("Failed to update thought");
       }
@@ -110,121 +115,293 @@ const ThoughtDetails = ({ token }) => {
 
   if (!thought) return <Typography>Loading...</Typography>;
 
+  //   return (
+  //     <Container maxWidth="md" sx={{ textAlign: "center" }}>
+  //       <Typography variant="h4" sx={{ mt: 3 }} gutterBottom>
+  //         Thought Details
+  //       </Typography>
+
+  //       {/* Display the thought name and created date */}
+  //       <Box
+  //         sx={{
+  //           backgroundColor: "#fff",
+  //           padding: 4,
+  //           borderRadius: 2,
+  //           boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+  //         }}
+  //       >
+  //         <Box sx={{ mb: 3 }}>
+  //           <Typography variant="h6">
+  //             Thought Name: {thought.thoughtName}
+  //           </Typography>
+  //           <Typography variant="body2" color="textSecondary">
+  //             Created on: {new Date(thought.created_date).toLocaleString()}
+  //           </Typography>
+  //         </Box>
+
+  //         {/* Form for updating thought details */}
+  //         <Box component="form" onSubmit={handleUpdate}>
+  //           <TextField
+  //             fullWidth
+  //             label="Edit Thought Name"
+  //             name="thoughtName"
+  //             value={formData.thoughtName}
+  //             multiline
+  //             onChange={handleChange}
+  //             margin="normal"
+  //           />
+  //           <TextField
+  //             fullWidth
+  //             label="Description"
+  //             name="Description"
+  //             value={formData.Description}
+  //             multiline
+  //             // minRows={10}
+  //             onChange={handleChange}
+  //             margin="normal"
+  //           />
+  //           <TextField
+  //             fullWidth
+  //             label="Emotions"
+  //             name="Emotions"
+  //             value={formData.Emotions}
+  //             onChange={handleChange}
+  //             margin="normal"
+  //             multiline
+  //           />
+  //           <TextField
+  //             fullWidth
+  //             label="Problems"
+  //             name="Problems"
+  //             value={formData.Problems}
+  //             onChange={handleChange}
+  //             margin="normal"
+  //             multiline
+  //           />
+  //           <TextField
+  //             fullWidth
+  //             label="Possible Solutions"
+  //             name="possibleSolutions"
+  //             value={formData.possibleSolutions}
+  //             onChange={handleChange}
+  //             margin="normal"
+  //             multiline
+  //           />
+  //           <TextField
+  //             fullWidth
+  //             label="Affirmation"
+  //             name="Affirmation"
+  //             value={formData.Affirmation}
+  //             onChange={handleChange}
+  //             margin="normal"
+  //             multiline
+  //           />
+
+  //           {/* Buttons for updating and deleting */}
+  //           <Grid
+  //             container
+  //             spacing={1}
+  //             sx={{ mt: 2, justifyContent: "flex-end" }}
+  //           >
+  //             <Grid item>
+  //               <Button
+  //                 type="submit"
+  //                 variant="contained"
+  //                 color="primary"
+  //                 sx={{
+  //                   mt: 2,
+  //                   backgroundColor: "#2c4e51",
+  //                   "&:hover": { backgroundColor: "#2c3e50" },
+  //                 }}
+  //               >
+  //                 Update Thought
+  //               </Button>
+  //             </Grid>
+  //             <Grid item>
+  //               <Button
+  //                 variant="outlined"
+  //                 color="error"
+  //                 sx={{ mt: 2 }}
+  //                 onClick={handleDelete}
+  //               >
+  //                 Delete Thought
+  //               </Button>
+  //             </Grid>
+  //           </Grid>
+  //         </Box>
+  //       </Box>
+  //     </Container>
+  //   );
+
   return (
-    <Container maxWidth="md" sx={{ textAlign: "center" }}>
-      <Typography variant="h4" sx={{ mt: 3 }} gutterBottom>
-        Thought Details
-      </Typography>
-
-      {/* Display the thought name and created date */}
-      <Box
-        sx={{
-          backgroundColor: "#fff",
-          padding: 4,
-          borderRadius: 2,
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
-        }}
+    <Container maxWidth="md">
+      <Grid
+        container
+        spacing={4}
+        textAlign="center"
+        alignItems="flex-start"
+        justifyContent={showDetails ? "flex-start" : "center"}
       >
-        <Box sx={{ mb: 3 }}>
-          <Typography variant="h6">
-            Thought Name: {thought.thoughtName}
+        <Grid item xs={12} md={showDetails ? 6 : 12}>
+          <Typography variant="h4" sx={{ mt: 3 }} gutterBottom>
+            Thought Details
           </Typography>
-          <Typography variant="body2" color="textSecondary">
-            Created on: {new Date(thought.created_date).toLocaleString()}
-          </Typography>
-        </Box>
-
-        {/* Form for updating thought details */}
-        <Box component="form" onSubmit={handleUpdate}>
-          <TextField
-            fullWidth
-            label="Edit Thought Name"
-            name="thoughtName"
-            value={formData.thoughtName}
-            multiline
-            onChange={handleChange}
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            label="Description"
-            name="Description"
-            value={formData.Description}
-            multiline
-            // minRows={10}
-            onChange={handleChange}
-            margin="normal"
-          />
-          <TextField
-            fullWidth
-            label="Emotions"
-            name="Emotions"
-            value={formData.Emotions}
-            onChange={handleChange}
-            margin="normal"
-            multiline
-          />
-          <TextField
-            fullWidth
-            label="Problems"
-            name="Problems"
-            value={formData.Problems}
-            onChange={handleChange}
-            margin="normal"
-            multiline
-          />
-          <TextField
-            fullWidth
-            label="Possible Solutions"
-            name="possibleSolutions"
-            value={formData.possibleSolutions}
-            onChange={handleChange}
-            margin="normal"
-            multiline
-          />
-          <TextField
-            fullWidth
-            label="Affirmation"
-            name="Affirmation"
-            value={formData.Affirmation}
-            onChange={handleChange}
-            margin="normal"
-            multiline
-          />
-
-          {/* Buttons for updating and deleting */}
-          <Grid
-            container
-            spacing={1}
-            sx={{ mt: 2, justifyContent: "flex-end" }}
+          {/* Display the thought name and created date */}
+          <Box
+            sx={{
+              backgroundColor: "#fff",
+              padding: 4,
+              borderRadius: 2,
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+            }}
           >
-            <Grid item>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                sx={{
-                  mt: 2,
-                  backgroundColor: "#2c4e51",
-                  "&:hover": { backgroundColor: "#2c3e50" },
-                }}
+            <Box sx={{ mb: 3 }}>
+              <Typography variant="h6">
+                Thought Name: {thought.thoughtName}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                Created on: {new Date(thought.created_date).toLocaleString()}
+              </Typography>
+            </Box>
+            <Box component="form" onSubmit={handleUpdate}>
+              <TextField
+                fullWidth
+                label="Edit Thought Name"
+                name="thoughtName"
+                value={formData.thoughtName}
+                multiline
+                onChange={handleChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Description"
+                name="Description"
+                value={formData.Description}
+                multiline
+                onChange={handleChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Emotions"
+                name="Emotions"
+                value={formData.Emotions}
+                onChange={handleChange}
+                margin="normal"
+                multiline
+              />
+              <TextField
+                fullWidth
+                label="Problems"
+                name="Problems"
+                value={formData.Problems}
+                onChange={handleChange}
+                margin="normal"
+                multiline
+              />
+              <TextField
+                fullWidth
+                label="Possible Solutions"
+                name="possibleSolutions"
+                value={formData.possibleSolutions}
+                onChange={handleChange}
+                margin="normal"
+                multiline
+              />
+              <TextField
+                fullWidth
+                label="Affirmation"
+                name="Affirmation"
+                value={formData.Affirmation}
+                onChange={handleChange}
+                margin="normal"
+                multiline
+              />
+              <Grid
+                container
+                spacing={1}
+                sx={{ mt: 2, justifyContent: "flex-end" }}
               >
-                Update Thought
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button
-                variant="outlined"
-                color="error"
-                sx={{ mt: 2 }}
-                onClick={handleDelete}
-              >
-                Delete Thought
-              </Button>
-            </Grid>
+                <Grid item>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                      mt: 2,
+                      backgroundColor: "#2c4e51",
+                      "&:hover": { backgroundColor: "#2c3e50" },
+                    }}
+                  >
+                    Add Details
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    sx={{ mt: 2 }}
+                    onClick={handleDelete}
+                  >
+                    Delete Thought
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+        {showDetails && (
+          <Grid item xs={12} md={6}>
+            <Typography variant="h4" sx={{ mt: 3 }} gutterBottom>
+              Added Details
+            </Typography>
+            <Box
+              sx={{
+                backgroundColor: "#f9f9f9",
+                padding: 4,
+                borderRadius: 2,
+                boxShadow: "0 2px 10px rgba(0, 0, 0, 0.1)",
+              }}
+            >
+              <Typography variant="h6">
+                <strong>Thought Name:</strong> {thought.thoughtName}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                Created on: {new Date(thought.created_date).toLocaleString()}
+              </Typography>
+              <Box sx={{ textAlign: "left" }}>
+                {thought.Description && (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    <strong>Description:</strong> {thought.Description}
+                  </Typography>
+                )}
+                {thought.Emotions.length > 0 && (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    <strong>Emotions:</strong> {thought.Emotions.join(", ")}
+                  </Typography>
+                )}
+                {thought.Problems.length > 0 && (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    <strong>Problems:</strong> {thought.Problems.join(", ")}
+                  </Typography>
+                )}
+                {thought.possibleSolutions.length > 0 && (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    <strong>Possible Solutions:</strong>{" "}
+                    {thought.possibleSolutions.join(", ")}
+                  </Typography>
+                )}
+                {thought.Affirmation && (
+                  <Typography variant="body1" sx={{ mt: 2 }}>
+                    <strong>Affirmation:</strong> {thought.Affirmation}
+                  </Typography>
+                )}
+              </Box>
+            </Box>
           </Grid>
-        </Box>
-      </Box>
+        )}
+      </Grid>
     </Container>
   );
 };
